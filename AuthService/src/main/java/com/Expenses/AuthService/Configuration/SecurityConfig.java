8cf339461ce5fab29927161bf0f0ae5f1f88a023
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-11-15 12:02:11 +0530

    resolve error

--- AuthService/src/main/java/com/Expenses/AuthService/Configuration/SecurityConfig.java
+++ AuthService/src/main/java/com/Expenses/AuthService/Configuration/SecurityConfig.java
@@ -18,31 +18,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity
-@Data
 public class SecurityConfig {
 
-    private final UserInfoRepository userInfoRepositoryl;
-    private final UserDetailServiceImpl userDetailService;
-    private  final JwtTokenService jwtTokenService;
+    private final UserDetailsService userDetailsService;
+    private final JwtTokenService jwtTokenService;
+    private final PasswordEncoder passwordEncoder;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(UserInfoRepository userInfoRepository, PasswordEncoder passwordEncoder){
-        return new UserDetailServiceImpl(userInfoRepository,passwordEncoder);
-    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
@@ -52,16 +42,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // disable CSRF for APIs
+                .csrf(csrf -> csrf.disable())
                 .cors(CorsConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/v1/login","/auth/v1/refresh-token","/auth/v1/signup ").permitAll() // public endpoints (e.g., signup/login)
-                        .anyRequest().authenticated() // everything else requires authentication
+                        .requestMatchers("/auth/v1/login","/auth/v1/refresh-token","/auth/v1/signup").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(sess ->sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .authenticationProvider(authenticationProvider())
-                .httpBasic(Customizer.withDefaults()); // enable basic auth for API testing
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
@@ -69,9 +59,8 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailService);
-        authProvider.setPasswordEncoder(passwordEncoder());
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
         return authProvider;
     }
-
 }
